be3cf46b9909ae4702c91f901ad23fdcb844b462
ClaudiaQuezadaRiquelme <cklaho.quesoda.X@gmail.com>
2019-11-05 18:37:01 -0300

    sine movement with bug

--- app.js
+++ app.js
@@ -1,44 +1,64 @@
 let globalID;
+let globalIDsin;
 
-// const time = {
-//   start: performance.now(),
-//   total: 6000
-// };
-
-const finalPosition = 75;
+const finalPosition = 75; // vh
+var xPos = 0;
+var yPos = 0;
+var angle = 0;
 
 const moveFromTopToDown = (location, time, elementID) => {
-  //console.log('moveFromTopToDown');
   
   let pos = location; // 0
   let setTime = time;
   let elemID = elementID;
   
   if (pos == finalPosition ) {
-    stopMove();
+    stopMove(globalID);
   } else {
     setTimeout(() => {
       pos++;
-      console.log('pos: ', pos);
+      // console.log('pos1: ', pos);
       document.getElementById(elemID).style.top = pos + 'vh';
-      //document.getElementById('cherryBlossom2').style.top = pos + 'vh';
-      //document.getElementById('cherryBlossom3').style.top = pos + 'vh';
       globalID = requestAnimationFrame(moveFromTopToDown(pos, setTime, elemID));
     }, setTime)
 
-    // pos++;
-    // console.log('pos: ', pos);
-    // document.getElementById('cherryBlossom').style.top = pos + 'vh';
-    // globalID = requestAnimationFrame(moveFromTopToDown(pos));
   }
 }
 
-// const startMove = (position) => {
-//   console.log('onclick works');
-//   globalID = requestAnimationFrame(moveFromTopToDown(position));
-// }
+const moveFromTopToDownSin = (location, time, elementID) => {
+  
+  let pos = location; // 0
+  let setTime = time;
+  let elemID = elementID;
+  
+  if (pos == finalPosition || pos > finalPosition ) {
+    stopMove(globalIDsin);
+  } else {
+    setTimeout(() => {
+      pos++;
+      xPos = Math.round(50 * Math.sin(angle));
+      angle += .1;
+      yPos += 5;
+      
+      document.getElementById(elemID).style.top = pos + 'vh';
+      document.getElementById(elemID).style.transform = `translate(${xPos}px, ${yPos}px)`;
 
-const stopMove = () => {
+      if (Math.abs(yPos) >= 900) {
+        yPos = -500;
+      }
+    
+      if (angle > 2 * Math.PI) {
+        angle = 0;
+      }
+
+
+      globalIDsin = requestAnimationFrame(moveFromTopToDownSin(pos, setTime, elemID));
+    }, setTime)
+
+  }
+}
+
+const stopMove = (ID) => {
   console.log('stopMove()');
-  cancelAnimationFrame(globalID);
+  cancelAnimationFrame(ID);
 }
