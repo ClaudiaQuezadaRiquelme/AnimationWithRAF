4792cbdff14c8b7cdc1880fb1beeed65f45bc573
ClaudiaQuezadaRiquelme <cklaho.quesoda.X@gmail.com>
2019-11-05 20:57:11 -0300

    fade and down

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let globalID;
 let globalIDsin;
+let globalIDsinFade;
 
 const finalPosition = 75; // vh
-var xPos = 0;
-var yPos = 0;
-var angle = 0;
+let xPos = 0;
+let angle = 0;
 
 const moveFromTopToDown = (location, time, elementID) => {
   
@@ -17,7 +17,6 @@ const moveFromTopToDown = (location, time, elementID) => {
   } else {
     setTimeout(() => {
       pos++;
-      // console.log('pos1: ', pos);
       document.getElementById(elemID).style.top = pos + 'vh';
       globalID = requestAnimationFrame(moveFromTopToDown(pos, setTime, elemID));
     }, setTime)
@@ -52,7 +51,65 @@ const moveFromTopToDownSin = (location, time, elementID) => {
   }
 }
 
+const fadeAndDown = (location, time, elementID, duration) => {
+  let pos = location; // 0
+  let setTime = time;
+  let elemID = elementID;
+  start = new Date;
+
+  if (pos == finalPosition || pos > finalPosition ) {
+    stopMove(globalIDsin);
+  } else {
+    setTimeout(() => {
+
+      if (pos >= 50) { //Fade
+        let time = new Date - start;
+        if(time < duration) {
+          document.getElementById(elemID).style.opacity = 1 - time / duration;
+          
+        } else {
+          document.getElementById(elemID).style.opacity = '0';
+        }
+      } else {
+        document.getElementById(elemID).style.opacity = '1';
+      }
+
+      pos++;
+      xPos = Math.round(50 * Math.sin(angle));
+      angle += .1;
+      
+      document.getElementById(elemID).style.top = pos + 'vh';
+      document.getElementById(elemID).style.transform = `translateX(${xPos}px)`;
+    
+      if (angle > 2 * Math.PI) {
+        angle = 0;
+      }
+
+      
+
+      globalIDsin = requestAnimationFrame(fadeAndDown(pos, setTime, elemID, duration));
+    }, setTime)
+
+  }
+
+}
+
 const stopMove = (ID) => {
   console.log('stopMove()');
   cancelAnimationFrame(ID);
+}
+
+
+
+const fade = (element, duration) => {
+  let start = new Date;
+  (next = () => {
+      let time = new Date - start;
+      if(time < duration) {
+          element.style.opacity = 1 - time / duration;
+          requestAnimationFrame(next());
+      } else {
+          element.style.opacity = '0';
+      }
+  })();
 }
